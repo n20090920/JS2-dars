3f10561a67be8c1cfbe06b00dd38ca0d09922dea
Nurjahon <166979887+n20090920@users.noreply.github.com>
2024-04-23 02:59:06 +0500

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,38 @@
+<!-- JS da promptdan foydalandim -->
+<!-- parseFloat dan ham foydalanganman -->
+
+Data types(ma'lumot turlari) 2 turga bo'linadi:
+1)Sodda(Primitive)
+null
+undefined
+number
+bigint
+string
+symbol
+
+2)Murakkab(Complex)
+object
+.
+typeof-----Biror bir ma'lumotni qaysi ma'lumot turidan ekanligini topish uchun ishlatiladi
+.
+Stringda yozishni 3 xil usuli bor:
+1)let backTicks = `To'rayev Nurjahon`;
+2)let doubleQuute = "To'rayev Nurjahon";
+3)let singleQuote = 'To'rayev Nurjahon';
+.
+NaN - so'zining to'liq shakli -- Not A Number
+.
+undefined - e'lon qilingan o'zgaruvchining qiymat berilmagandagi holati
+.
+boolean da faqat 2 xil qiymat bo'ladi :
+true(to'g'ri) va false(Xato)
+PI -- Math.PI()
+Modul -- Math.abs()
+Ildiz osti -- Math.sqrt()
+.
+parseFloat -- string ni number ga aylantirishga yordam beradi
+.
+Math.trunc(x) - x ning butun qismini qaytaradi
+.
+Math.random() - 0 va 1 orasidagi random sonlar
+# JS2-dars
---
+++ main.js
@@ -0,0 +1,124 @@
+// JS da promptdan foydalandim
+// parseFloat dan ham foydalanganman
+/*
+.
+.
+.
+.
+*/
+////////////1-misol/////////
+/*
+let A = prompt("A ning qiymatini kiriting");
+let B = prompt("B ning qiymatini kiriting");
+let Y = -(B / 2) * (B ** 2 + A ** 2);
+console.log(`Javob: ${Y}`);
+*/
+////////////2-misol/////////
+/*
+let A = prompt("A ning qiymatini kiriting");
+let B = prompt("B ning qiymatini kiriting");
+let C = prompt("C ning qiymatini kiriting");
+let D = prompt("D ning qiymatini kiriting");
+A = parseFloat(A);
+B = parseFloat(B);
+C = parseFloat(C);
+D = parseFloat(D);
+let Y = ((A + B) / (C + D)) ** 2 + B ** 2;
+console.log(`Javob: ${Y}`);
+*/
+////////////3-misol/////////
+/*
+let A = prompt("A ning qiymatini kiriting");
+let C = prompt("C ning qiymatini kiriting");
+let D = prompt("D ning qiymatini kiriting");
+A = parseFloat(A);
+C = parseFloat(C);
+D = parseFloat(D);
+let Y = 2 * (A ** D + (4 * C ** 2) / 3);
+console.log(`Javob: ${Y}`);
+*/
+////////////4-misol/////////
+/*
+let A = prompt("A ning qiymatini kiriting");
+let B = prompt("B ning qiymatini kiriting");
+let C = prompt("C ning qiymatini kiriting");
+let D = prompt("D ning qiymatini kiriting");
+A = parseFloat(A);
+B = parseFloat(B);
+C = parseFloat(C);
+D = parseFloat(D);
+let Y = (1 / A ** 2) * (B / 10) ** 3 * (C + D) ** 2;
+console.log(`Javob: ${Y}`);
+*/
+////////////5-misol/////////
+/*
+let A = prompt("A ning qiymatini kiriting");
+let B = prompt("B ning qiymatini kiriting");
+let C = prompt("C ning qiymatini kiriting");
+let D = prompt("D ning qiymatini kiriting");
+A = parseFloat(A);
+B = parseFloat(B);
+C = parseFloat(C);
+D = parseFloat(D);
+let Y = ((A / B - 1) / (C / (D - 1))) ** 2;
+console.log(`Javob: ${Y}`);
+*/
+////////////6-misol/////////
+/*
+let A = prompt("A ning qiymatini kiriting");
+let B = prompt("B ning qiymatini kiriting");
+let C = prompt("C ning qiymatini kiriting");
+let D = prompt("D ning qiymatini kiriting");
+A = parseFloat(A);
+B = parseFloat(B);
+C = parseFloat(C);
+D = parseFloat(D);
+let Y = (A + B) / (C + D / (A + C));
+console.log(`Javob: ${Y}`);
+*/
+////////////7-misol/////////
+/*
+let A = prompt("A ning qiymatini kiriting");
+let B = prompt("B ning qiymatini kiriting");
+let C = prompt("C ning qiymatini kiriting");
+A = parseFloat(A);
+B = parseFloat(B);
+C = parseFloat(C);
+let Y = Math.sqrt((A + B + C) ** 2 - (A - B - C) ** 2);
+console.log(Y);
+*/
+////////////8-misol/////////
+/*
+let A = prompt("A ning qiymatini kiriting");
+let B = prompt("B ning qiymatini kiriting");
+let C = prompt("C ning qiymatini kiriting");
+A = parseFloat(A);
+B = parseFloat(B);
+C = parseFloat(C);
+let Y = (A ** 2 + B ** 2 + C ** 2) / (A * B * C);
+console.log(`Javob: ${Y}`);
+*/
+////////////9-misol/////////
+/*
+let A = prompt("A ning qiymatini kiriting");
+let B = prompt("B ning qiymatini kiriting");
+let C = prompt("C ning qiymatini kiriting");
+let D = prompt("D ning qiymatini kiriting");
+A = parseFloat(A);
+B = parseFloat(B);
+C = parseFloat(C);
+D = parseFloat(D);
+let Y = A * (D ** 3 / 3) + B * (C ** 2 / 2);
+console.log(`Javob: ${Y}`);
+*/
+////////////10-misol////////
+/*
+let A = prompt("A ning qiymatini kiriting");
+let B = prompt("B ning qiymatini kiriting");
+let C = prompt("C ning qiymatini kiriting");
+A = parseFloat(A);
+B = parseFloat(B);
+C = parseFloat(C);
+let Y = Math.abs(A / B - B / C) + (A + C) ** 0.25;
+console.log(`Javob: ${Y}`);
+*/
